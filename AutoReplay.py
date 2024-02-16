9999e6690a69c4b02ce1b45351d974c4b4ab4e01
Dyvinia <dulana57@gmail.com>
2024-02-15 20:53:08 -0500

    add options

--- AutoReplay.py
+++ AutoReplay.py
@@ -11,11 +11,32 @@ def script_description():
 
 Author: Dyvinia"""
 
+def script_properties():
+    props = obs.obs_properties_create()
+    scene_for_clips = obs.obs_properties_add_list(props, "scene", "Scene For Clipping",
+                                    obs.OBS_COMBO_TYPE_EDITABLE,
+                                    obs.OBS_COMBO_FORMAT_STRING)
+    scenes = obs.obs_frontend_get_scene_names()
+    
+    for scene in scenes:
+        obs.obs_property_list_add_string(scene_for_clips, scene, scene)
+
+    obs.source_list_release(scenes)
+
+    obs.obs_properties_add_bool(props, "disabled", "Disable Clipping")
+    obs.obs_properties_add_bool(props, "disable_notif", "Disable Notification On Save")
+
+    return props
+
 toaster = WindowsToaster('OBS Replay')
+sett = None
 
 def script_load(settings):
     obs.obs_frontend_add_event_callback(obs_frontend_callback)
     obs.timer_add(auto_replay_buffer, 10000)
+
+    global sett
+    sett = settings
 
 def script_unload():
     obs.timer_remove(auto_replay_buffer)
@@ -24,11 +45,12 @@ def script_unload():
 def obs_frontend_callback(event):
     if event == obs.OBS_FRONTEND_EVENT_REPLAY_BUFFER_SAVED:
         path = move_recording()
-        newToast = Toast()
-        newToast.text_fields = ['Saved Replay', "Saved in " + path]
-        newToast.duration = ToastDuration.Short
-        toaster.clear_toasts()
-        toaster.show_toast(newToast)
+        if not obs.obs_data_get_bool(sett, "disable_notif"):
+            newToast = Toast()
+            newToast.text_fields = ['Saved Replay', "Saved in " + path]
+            newToast.duration = ToastDuration.Short
+            toaster.clear_toasts()
+            toaster.show_toast(newToast)
     elif event == obs.OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTED:
         newToast = Toast()
         newToast.text_fields = ['Started Replay Buffer']
@@ -43,22 +65,28 @@ def obs_frontend_callback(event):
         toaster.show_toast(newToast)
 
 def auto_replay_buffer():
+    if obs.obs_data_get_bool(sett, "disabled"):
+        if obs.obs_frontend_replay_buffer_active():
+            obs.obs_frontend_replay_buffer_stop()
+        return
+
     scene_as_source = obs.obs_frontend_get_current_scene()
+
+    if obs.obs_source_get_name(scene_as_source) != obs.obs_data_get_string(sett, "scene"):
+        obs.obs_source_release(scene_as_source)
+        return
+
     current_scene = obs.obs_scene_from_source(scene_as_source)
     scene_item = obs.obs_scene_find_source_recursive(current_scene, "Fullscreen Game Capture")
     source = obs.obs_sceneitem_get_source(scene_item)
 
     if (not obs.obs_frontend_replay_buffer_active() and obs.obs_source_get_width(source) > 0):
-        #print("Starting Replay Buffer")
         obs.obs_frontend_replay_buffer_start()
         
     elif (obs.obs_frontend_replay_buffer_active() and obs.obs_source_get_width(source) == 0):
-        #print("Stopping Replay Buffer")
         obs.obs_frontend_replay_buffer_stop()
 
     obs.obs_source_release(scene_as_source)
-
-    #print(get_foreground_window())
 
 def move_recording():
     replay_buffer = obs.obs_frontend_get_replay_buffer_output()
